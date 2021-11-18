979c7eaa056ef6f4283a8a24b66edceeaae5e3eb
niksON <gromyakov05@mail.ru>
2021-11-19 00:02:58 +0300

    test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Telegraf, Markup } = require('telegraf')
 require('dotenv').config()
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const bot = new Telegraf('2102861921:AAHHYJSC6l0ZmtkRny2J3CH6HUdM66TlukA')
 bot.start((ctx) => {
   ctx.reply(
     'Привет!'
